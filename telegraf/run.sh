5da65b0355f5304e1c78a5dfe47c83f85107fbb3
adam <pr0bsh@gmail.com>
2017-09-07 22:44:36 -0500

    replaced with stock image

--- telegraf/run.sh
+++ telegraf/run.sh
@@ -1,15 +1,8 @@
-#!/bin/bash
+#!/bin/sh
+set -e
 
-set -m
-CONFIG_TEMPLATE="/telegraf.template.conf"
-CONFIG_FILE="/etc/telegraf/telegraf.conf"
+if [ "${1:0:1}" = '-' ]; then
+    set -- telegraf "$@"
+fi
 
-sed -e "s/\${HOST_NAME}/$HOST_NAME/" \
- -e "s!\${INFLUXDB_HOST}!$INFLUXDB_HOST!" \
- -e "s/\${INFLUXDB_PORT}/$INFLUXDB_PORT/" \
- -e "s/\${DATABASE}/$DATABASE/" \
- $CONFIG_TEMPLATE > $CONFIG_FILE
-
-echo "=> Starting Telegraf ..."
-
-exec telegraf -config /etc/telegraf/telegraf.conf
+exec "$@"
