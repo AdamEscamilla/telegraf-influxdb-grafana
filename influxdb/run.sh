f3c32692c4c49a07a91cb15086a060c0626bf3d1
adam <pr0bsh@gmail.com>
2017-09-07 22:44:04 -0500

    using smaller image

--- influxdb/run.sh
+++ influxdb/run.sh
@@ -1,32 +1,29 @@
-#!/bin/bash
+#!/bin/sh
 
-set -m
-CONFIG_TEMPLATE="/influxdb.template.conf"
-CONFIG_FILE="/etc/influxdb/influxdb.conf"
-CURR_TIMESTAMP=`date +%s`
+set -e
 
-INFLUX_HOST="localhost"
-INFLUX_API_PORT="8086"
-[ "${INFLUX_ADMIN_USER}" = "" ] &&
-        INFLUX_ADMIN_USER="grafana"
-[ "${INFLUX_ADMIN_PASS}" = "" ] &&
-        INFLUX_ADMIN_PASS="grafana"
-[ "${INFLUX_DATABASE}" = "" ] &&
-        JENKINS_BIN="telegraf"
+if [ "${1:0:1}" = '-' ]; then
+    set -- influxd "$@"
+fi
 
-mv -v $CONFIG_FILE $CONFIG_FILE.$CURR_TIMESTAMP
-cp -v $CONFIG_TEMPLATE $CONFIG_FILE
+exec
 
-exec influxd -config=$CONFIG_FILE 1>>/var/log/influxdb/influxdb.log 2>&1 &
+"$@" &
+
 sleep 5
 
-USER_EXISTS=`influx -host=${INFLUX_HOST} -port=${INFLUX_API_PORT} -execute="SHOW USERS" | awk '{print $1}' | grep "${INFLUX_ADMIN_USER}" | wc -l`
+INFLUXDB_HOST="localhost"
+INFLUXDB_API_PORT="8086"
+INFLUXDB_ADMIN_USER=${INFLUXDB_ADMIN_USER:-grafana}
+INFLUXDB_ADMIN_PASS=${INFLUXDB_ADMIN_PASS:-grafana}
+INFLUXDB_DATABASE=${INFLUXDB_DATABASE:-telegraf}
 
-if [ -n ${USER_EXISTS} ]
-then
-  influx -host=${INFLUX_HOST} -port=${INFLUX_API_PORT} -execute="CREATE USER ${INFLUX_ADMIN_USER} WITH PASSWORD '${INFLUX_ADMIN_PASS}' WITH ALL PRIVILEGES"
-  influx -host=${INFLUX_HOST} -port=${INFLUX_API_PORT} -username=${INFLUX_ADMIN_USER} -password="${INFLUX_ADMIN_PASS}" -execute="create database ${INFLUX_DATABASE}"
-  influx -host=${INFLUX_HOST} -port=${INFLUX_API_PORT} -username=${INFLUX_ADMIN_USER} -password="${INFLUX_ADMIN_PASS}" -execute="grant all PRIVILEGES on ${INFLUX_DATABASE} to ${INLFUX_ADMIN_USER}"
-fi 
+USER_EXISTS=`influx -host=${INFLUXDB_HOST} -port=${INFLUXDB_API_PORT} -execute="SHOW USERS" | awk '{print $1}' | grep "${INFLUXDB_ADMIN_USER}" | wc -l`
 
-tail -f /var/log/influxdb/influxdb.log
+if [ -n ${USER_EXISTS} ]; then
+  influx -host=${INFLUXDB_HOST} -port=${INFLUXDB_API_PORT} -execute="CREATE USER ${INFLUXDB_ADMIN_USER} WITH PASSWORD '${INFLUXDB_ADMIN_PASS}' WITH ALL PRIVILEGES"
+  influx -host=${INFLUXDB_HOST} -port=${INFLUXDB_API_PORT} -username=${INFLUXDB_ADMIN_USER} -password="${INFLUXDB_ADMIN_PASS}" -execute="create database ${INFLUXDB_DATABASE}"
+  influx -host=${INFLUXDB_HOST} -port=${INFLUXDB_API_PORT} -username=${INFLUXDB_ADMIN_USER} -password="${INFLUXDB_ADMIN_PASS}" -execute="grant all PRIVILEGES on ${INFLUXDB_DATABASE} to ${INFLUXDB_ADMIN_USER}"
+fi
+
+wait
